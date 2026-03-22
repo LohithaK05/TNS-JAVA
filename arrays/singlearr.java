554acd9fd2137f865cf9eccd25194d652758858a
Lohitha <lohithakamuju2005@gmail.com>
2026-03-22 20:05:30 +0530

    arrays

---
+++ arrays/singlearr.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class singlearr {
+
+	public static void main(String[] args) {
+		int[] arr = new int[3];
+		
+		 arr[0] = 1;
+		 arr[1] = 2;
+		 arr[2] = 3;
+		 
+		 System.out.println(arr[2]);  //3
+		 System.out.println(arr[1]);  //2
+		 //System.out.println(arr[4]);  //ArrayIndexOutofBoundsException
+		 
+	}
+
+}
